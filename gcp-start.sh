7d20d884aeda5ca4e1e5ee5e86156b6d9f567a1f
Chuck Findlay <chuck@findlayis.me>
2024-01-28 21:02:40 -0400

    initial setup - need to flesh out start cmd and gcp-start

---
+++ gcp-start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# Author: Chuck Findlay <chuck@findlayis.me>
+# License: LGPL v3.0
+
+## do stuff
+
+/run.sh
---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+# Author: Chuck Findlay <chuck@findlayis.me>
+# License: LGPL v3.0
+
+/steam/steamcmd.sh +force_install_dir /projectzomboid +login anonymous +app_update 380870 validate +quit
