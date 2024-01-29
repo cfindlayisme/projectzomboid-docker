bf83164f3d8f6ee50ceba9c1f97b58c8e677d638
Chuck Findlay <chuck@findlayis.me>
2024-01-28 22:48:06 -0400

    cron stuff for gcp script

--- gcp-start.sh
+++ gcp-start.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 #
+# Start script handler for GCP projectzomboid-docker image
+#
 # Author: Chuck Findlay <chuck@findlayis.me>
 # License: LGPL v3.0
+
+echo "0 */1 * * * gcloud auth activate-service-account --key-file=/config/gcs-key.json && gsutil cp /config/world.zip gs://$GCS_BUCKET$GCS_BUCKET_PATH/world.zip" > /etc/cron.d/gcs-hourly-backup
+chmod 0644 /etc/cron.d/gcs-hourly-backup
+crontab /etc/cron.d/gcs-hourly-backup
+
+# Start cron so it runs auto backups
+cron
 
 ## do stuff
 
