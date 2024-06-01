72b33fa0d90beec849bf9da9446c970737518781
Chuck Findlay <chuck@findlayis.me>
2024-06-01 03:38:46 -0300

    set it up so we can adjust rconport and rconpassword from env variables

--- run.sh
+++ run.sh
@@ -6,5 +6,15 @@
 # Update/install server on startup
 /steam/steamcmd.sh +force_install_dir /projectzomboid +login anonymous +app_update 380870 validate +quit
 
+# Replace RCONPort and RCONPassword in the ini file with environment variables
+for file in /root/Zomboid/Server/*.ini
+do
+    sed -i "s/^RCONPort=.*/RCONPort=${RCONPort}/" "$file"
+    sed -i "s/^RCONPassword=.*/RCONPassword=${RCONPassword}/" "$file"
+    echo "Updated $file:"
+    echo "RCONPort is now set to ${RCONPort}"
+    echo "RCONPassword is now set to ${RCONPassword}"
+done
+
 # config files in /root/Zomboid
 /projectzomboid/start-server.sh
